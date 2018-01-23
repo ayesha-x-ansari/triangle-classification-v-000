087ecde0d0e25dab260d750e39bde992db257e28
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-23 22:04:37 +0000

    automatically backed up by learn

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -48,15 +48,15 @@ describe 'Triangle' do
     expect{Triangle.new(3, 4, -5).kind}.to raise_error(TriangleError)
   end
 
-  xit 'knows that triangles violating triangle inequality are illegal' do
+  it 'knows that triangles violating triangle inequality are illegal' do
     expect{Triangle.new(1, 1, 3).kind}.to raise_error(TriangleError)
   end
 
-  xit 'knows that triangles violating triangle inequality are illegal 2' do
+  it 'knows that triangles violating triangle inequality are illegal 2' do
     expect{Triangle.new(2, 4, 2).kind}.to raise_error(TriangleError)
   end
 
-  xit 'knows that triangles violating triangle inequality are illegal 3' do
+  it 'knows that triangles violating triangle inequality are illegal 3' do
     expect{Triangle.new(7, 3, 2).kind}.to raise_error(TriangleError)
   end
 
