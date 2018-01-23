a6c3403ce5c43fc62b69515632c1a706012e3c53
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-23 21:38:10 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,49 @@
 class Triangle
-  # write code here
+  attr_accessor :value1, :value2, :value3
+
+  def initialize(value1, value2, value3)
+    @value1 = value1
+    @value2 = value2
+    @value3 = value3
+  end
+
+  def kind
+    validate_triangle
+    if @value1 == @value2 &&   @value2 == @value3
+       :equilateral
+    elsif @value1 == @value2 ||   @value2 == @value3  ||  @value1 == @value3
+       :isosceles
+    else
+       :scalene
+    end
+  end
+
+  def validate_triangle
+    if (value1 + value2 > value3 && value2 + value3 > value1 && value2 + value3 > value1
+          valid_triangle = [value1, value2 value3].collect do |value| "false" if value <= 0 }
+            puts valid_triangle
+            if valid_triangle.iclude?(false)
+              begin
+                raise TriangleError
+              rescue TriangleError => error
+                  puts error.message
+              end
+            end
+    end
+  end
 end
+
+class TriangleError < StandardError
+#  def message
+#    "Cant create triangle with parameter passed"
+#  end
+end
+
+triangle = Triangle.new(1,1,-3)
+triangle.kind
+#triangle1 = Triangle.new(7,3,1)
+#triangle1.kind
+#triangle2 = Triangle.new(4,1,2)
+#triangle2.kind
+#triangle2 = Triangle.new(0,0,0)
+#triangle2.kind
